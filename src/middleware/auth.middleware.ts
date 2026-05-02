d95ed5e3e56a66c6e0e601d04652b9e0806a5a94
Shivam Karn <shivamkarn767@gmail.com>
2026-05-02 17:54:42 +0545

    Auth check middleware complete.

---
+++ src/middleware/auth.middleware.ts
@@ -0,0 +1,25 @@
+import { createMiddleware } from "hono/factory";
+import { getBetterAuthInstance } from "../lib/auth";
+import type { BindingsType } from "../lib/types";
+import type { User } from "../lib/types";
+
+const authCheck = createMiddleware<{
+  Bindings: BindingsType;
+  Variables: { user: User };
+}>(async (c, next) => {
+  const auth = getBetterAuthInstance(
+    c.env.password_manager_db,
+    c.env.BETTER_AUTH_SECRET,
+  );
+  const session = await auth.api.getSession({ headers: c.req.raw.headers });
+
+  if (!session) {
+    return c.json({ message: "Unauthorized Request" }, 401);
+  }
+
+  c.set("user", session.user);
+
+  await next();
+});
+
+export { authCheck };
