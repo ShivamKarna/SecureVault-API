55781e1632d658ddbd99ad41f8f3bf1b81f25377
Shivam Karn <shivamkarn767@gmail.com>
2026-05-03 07:32:22 +0545

    Configured routes.
    Changed some files names. password->vault

--- src/controllers/password.controller.ts
+++
@@ -1,7 +0,0 @@
-import { factory } from "../lib/factory";
-
-const getPasswords = factory.createHandlers(async (c) => {
-  return c.json({ message: "Hey this is getPasswords Route" }, 200);
-});
-
-export { getPasswords };
--- src/routes/vault.routes.ts
+++ src/routes/vault.routes.ts
@@ -1,18 +1,18 @@
 import { Hono } from "hono";
-import { getPasswords } from "../controllers/password.controller";
 import { SmartRouter } from "hono/router/smart-router";
 import { RegExpRouter } from "hono/router/reg-exp-router";
 import { TrieRouter } from "hono/router/trie-router";
 import { authCheck } from "../middleware/auth.middleware";
+import { vaultController } from "../controllers/vault.controller";
 
-const passwordRouter = new Hono({
+const vaultRouter = new Hono({
   router: new SmartRouter({
     routers: [new RegExpRouter(), new TrieRouter()],
   }),
 });
 
 // so api link will be : localhost/api/passwords/**
-passwordRouter.use("/*", authCheck);
-passwordRouter.get("/", ...getPasswords);
+vaultRouter.use("/*", authCheck);
+vaultRouter.get("/", ...vaultController.getVaults);
 
-export default passwordRouter;
+export default vaultRouter;
