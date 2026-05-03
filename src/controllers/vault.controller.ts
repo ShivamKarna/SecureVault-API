170d98de4df78fa5d29a1898c7c252f865e1ab15
Shivam Karn <shivamkarn767@gmail.com>
2026-05-03 07:34:00 +0545

    Completed vault CRUD.
    Not all functions but mostly all of the are ready.

---
+++ src/controllers/vault.controller.ts
@@ -0,0 +1,166 @@
+import { and, eq } from "drizzle-orm";
+import { getDb } from "../db";
+import { vaultEntries } from "../db/schema";
+import { factory } from "../lib/factory";
+import { parse, z } from "zod";
+import { username } from "better-auth/plugins";
+import { encryptPassword } from "../lib/encryption";
+
+export class VaultController {
+  getVaults = factory.createHandlers(async (c) => {
+    const db = getDb(c.env.password_manager_db);
+    const user = c.get("user");
+
+    const result = await db
+      .select()
+      .from(vaultEntries)
+      .where(eq(vaultEntries.userId, user.id));
+
+    return c.json({ result }, 200);
+  });
+  getVaultById = factory.createHandlers(async (c) => {
+    const id = c.req.param("id");
+    if (!id) {
+      return c.json({ error: "ID is required" }, 400);
+    }
+    const db = getDb(c.env.password_manager_db);
+    const user = c.get("user");
+
+    const result = await db
+      .select()
+      .from(vaultEntries)
+      .where(and(eq(vaultEntries.id, id), eq(vaultEntries.userId, user.id)))
+      .get(); // get makes sure that it returns the first row or undefined
+
+    return c.json({ result }, 200);
+  });
+  createVault = factory.createHandlers(async (c) => {
+    const userInput = await c.req.json();
+    const db = getDb(c.env.password_manager_db);
+    const user = c.get("user");
+
+    const schema = z.object({
+      label: z.string().min(2).max(200),
+      username: z.string().min(2).max(30),
+      password: z.string().min(7).max(60),
+      url: z.string().optional(),
+      notes: z.string().optional(),
+    }); //zod parsing
+
+    const parsed = schema.safeParse(userInput);
+    if (!parsed.success) {
+      return c.json({ error: parsed.error.issues }, 400);
+    }
+
+    const encryptedPassword = await encryptPassword(
+      parsed.data.password,
+      c.env.ENCRYPTION_KEY,
+    );
+
+    const acknowlegement = await db
+      .insert(vaultEntries)
+      .values({
+        userId: user.id,
+        label: parsed.data.label,
+        username: parsed.data.username,
+        encryptedPassword,
+        url: parsed.data.url ?? null,
+        notes: parsed.data.notes ?? null,
+      })
+      .returning();
+
+    if (!acknowlegement) {
+      return c.json({ message: "Something went Wrong" }, 500);
+    }
+
+    return c.json({ acknowlegement }, 201);
+  });
+  updateVaultEntries = factory.createHandlers(async (c) => {
+    const db = getDb(c.env.password_manager_db);
+
+    const user = c.get("user");
+    const vaultId = c.req.param("id");
+    if (!vaultId) {
+      return c.json({ error: "ID is required" }, 400);
+    }
+    const existenceProof = await db
+      .select()
+      .from(vaultEntries)
+      .where(
+        and(eq(vaultEntries.id, vaultId), eq(vaultEntries.userId, user.id)),
+      )
+      .get();
+
+    if (!existenceProof) {
+      return c.json({ message: "Vault Entry Not Found" }, 404);
+    }
+
+    const newValues = await c.req.json();
+    const schema = z
+      .object({
+        label: z.string().min(2).max(200),
+        username: z.string().min(2).max(30),
+        password: z.string().min(7).max(60),
+        url: z.string().optional(),
+        notes: z.string().optional(),
+      })
+      .partial(); //zod parsing
+
+    const parsed = schema.safeParse(newValues);
+    if (!parsed.success) {
+      return c.json({ error: parsed.error.issues }, 400);
+    }
+    const encryptedPassword = parsed.data.password
+      ? await encryptPassword(parsed.data.password, c.env.ENCRYPTION_KEY)
+      : undefined;
+
+    const result = await db
+      .update(vaultEntries)
+      .set({
+        label: parsed.data.label,
+        username: parsed.data.username,
+        encryptedPassword,
+        url: parsed.data.url,
+        notes: parsed.data.notes,
+      })
+      .where(
+        and(eq(vaultEntries.id, vaultId), eq(vaultEntries.userId, user.id)),
+      )
+      .returning();
+
+    if (!result.length) {
+      return c.json({ message: "Something went wrong" }, 500);
+    }
+    return c.json({ result }, 200);
+  });
+  deleteVault = factory.createHandlers(async (c) => {
+    const db = getDb(c.env.password_manager_db);
+    const user = c.get("user");
+    const vaultId = c.req.param("id");
+
+    if (!vaultId) {
+      return c.json({ message: "Id not provided" }, 400);
+    }
+
+    const existing = await db
+      .select()
+      .from(vaultEntries)
+      .where(
+        and(eq(vaultEntries.id, vaultId), eq(vaultEntries.userId, user.id)),
+      )
+      .get();
+
+    if (!existing) {
+      return c.json({ message: "Vault Entry not found" }, 404);
+    }
+
+    await db
+      .delete(vaultEntries)
+      .where(
+        and(eq(vaultEntries.id, vaultId), eq(vaultEntries.userId, user.id)),
+      );
+    return c.json({ message: "Vault Deleted Successfully" }, 200);
+  });
+}
+
+export const vaultController = new VaultController();
