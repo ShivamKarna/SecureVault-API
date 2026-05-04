fe5db4fc49ba0ec0888a6c31c0d71b7f5a1462e4
Shivam Karn <shivamkarn767@gmail.com>
2026-05-04 14:26:38 +0545

    Added cors support.

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,32 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { getBetterAuthInstance } from "./lib/auth";
 import mainRouter from "./routes";
-import type { BindingsType } from "./lib/types";
+import type { BindingsType, User } from "./lib/types";
+import { rateLimit } from "./middleware/rateLimiter.middleware";
 
-const app = new Hono<{ Bindings: BindingsType }>();
+const app = new Hono<{ Bindings: BindingsType; Variables: { user: User } }>();
+
+app.use(
+  "*",
+  cors({
+    origin: ["vault.shivam-karn.com.np"],
+    allowMethods: ["GET", "POST", "DELETE", "PATCH"],
+    allowHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }),
+);
 
 app.get("/", (c) => {
-  return c.text("Hello Hono!");
+  return c.text("Welcome to SecureVault API !");
 });
+app.use("/api/*", rateLimit);
 
 app.on(["GET", "POST"], "/api/auth/**", (c) => {
   const auth = getBetterAuthInstance(
     c.env.securevault_db,
     c.env.BETTER_AUTH_SECRET,
+    c.env,
   );
   return auth.handler(c.req.raw);
 });
