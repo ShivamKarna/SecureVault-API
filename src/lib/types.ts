627a950af70d93ca065c85f14b820a52bbf351be
Shivam Karn <shivamkarn767@gmail.com>
2026-05-04 14:28:58 +0545

    Added BIndings types for KV and github/google Oauth.

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,6 +2,12 @@ export type BindingsType = {
   securevault_db: D1Database;
   BETTER_AUTH_SECRET: string;
   ENCRYPTION_KEY: string;
+  RATE_LIMIT: KVNamespace;
+  GOOGLE_CLIENT_ID: string;
+  GOOGLE_CLIENT_SECRET: string;
+  GITHUB_CLIENT_ID: string;
+  GITHUB_CLIENT_SECRET: string;
+  BETTER_AUTH_URL: string;
 };
 
 export type User = {
